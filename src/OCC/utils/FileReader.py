173a6d1a52fc641d7a5b1e7c80e0736d16269466
mutawalle <13521065@std.stei.itb.ac.id>
2023-12-01 13:48:50 +0700

    fix: file reader

--- src/OCC/utils/FileReader.py
+++ src/OCC/utils/FileReader.py
@@ -2,8 +2,9 @@ def read(filepath):
     with open(filepath, 'r') as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines]
+        divider = lines.index("---- BEGIN EXECUTION ----")
 
-        operations = lines
+        operations = lines[divider + 1:]
         j = 0
         for i in range(len(operations)):
             if(len(operations[j])<5):
